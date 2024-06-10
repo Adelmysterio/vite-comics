3480018de875438a0ad0ae858bb1bb4f48775c3f
AdelMysterio <storliniadelmo@gmail.com>
2024-06-10 14:08:49 +0200

    created scaffolding

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use './style/general.sass' as *
+</style>
