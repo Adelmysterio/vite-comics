da3e230be6f42e0d57ca9e5953115ed25e0b4196
AdelMysterio <storliniadelmo@gmail.com>
2024-06-10 18:07:33 +0200

    implemented AppFooterJumbo

--- src/components/AppFooterJumbo.vue
+++ src/components/AppFooterJumbo.vue
@@ -12,8 +12,8 @@ export default {
     <div class="bg">
         <div class="container">
             <button>sign-up now!</button>
-            <nav>
-                <p>follow us</p>
+            <nav class="flex-centered">
+                <h3>follow us</h3>
                 <section>
                     <a href=""><img src="../assets/img/footer-facebook.png" alt=""></a>
                     <a href=""><img src="../assets/img/footer-twitter.png" alt=""></a>
@@ -31,5 +31,35 @@ export default {
 
 div.bg {
     background-color: $dc-gray;
+    div{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        padding: 2rem 0;
+        
+        h3 {
+            color: $dc-blue;
+            text-transform: uppercase;
+            margin-right: 1.5rem;
+            font-size: 1.4rem;
+        }
+
+        a {
+            margin-right: 1rem;
+        }
+    }
+
+    button {
+        border: 2px solid $dc-blue;
+        background-color: transparent;
+        font-family: "Roboto", sans-serif;
+        font-weight: bold;
+        font-size: 1.2rem;
+        color: white;
+        text-transform: uppercase;
+        padding: .8rem;
+    }
 }
+
+
 </style>
